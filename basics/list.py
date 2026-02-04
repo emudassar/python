4274508f40631e9562636bf9c907a78ffa043e3a
M Mudassar <contacmudassar@gmail.com>
2026-02-04 12:45:25 +0500

    practice list

---
+++ basics/list.py
@@ -0,0 +1,22 @@
+number = [1, 2, 3, 4, 5]
+
+# print(number[-2])
+# print(number[1:4])
+
+# number[3] = 6
+
+# print(number[1:4])
+
+# for number in number:
+#   print(number)
+
+number.append(7)
+
+# if 7 in number:
+#   print("I have seven")
+
+number.pop()
+
+print(number)
+
+
