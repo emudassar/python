88fafe3c9d6ef11db16fa80372f8b2f17a4057ee
M Mudassar <contacmudassar@gmail.com>
2026-02-18 15:55:24 +0500

    Arrays in python

---
+++ arrays/array.py
@@ -0,0 +1,55 @@
+# from array import *
+
+# val = array('i', [1,2,3,4,5,6,7,8,9])
+
+# for i in range(0, len(val)):
+#   print(val[i], end=" ")
+
+# print("\n")
+
+# for i in val:
+#   print(i, end=" ")
+
+# print("\n")
+
+# copyArray = array(val.typecode, (x for x in val))
+
+# for x in copyArray:
+#   print(x, end=" ")
+
+# print("\n")
+
+# val.reverse()
+
+# val.insert(1, 10)
+
+# val.append(11)
+  
+# for x in val:
+#   print(x, end=" ")
+
+# val.pop()
+# val.pop(3)
+
+# val.remove(7)
+
+# newArray = val[2 : -1]
+
+# newArray = val[::-1]
+
+# i = array.index()
+
+# n = int(input('Enter a number: '))
+
+# for i in range(0, n):
+#   val.append(int(input('Enter next number: ')))
+
+# for x in val:
+#   print(x, end=" ")
+
+from numpy import *
+
+val = array([1, 2, 3, 4])
+
+for i in range(0, len(val)):
+  print(val[i], end=" ")
